b84f222c7773ff65f0d9c89baba70a2b5124e727
El Popkova <111616351+elzifer@users.noreply.github.com>
2023-07-08 04:19:23 +0300

    work 8 for 5 balls done

--- bjs/08_if_else/script.js
+++ bjs/08_if_else/script.js
@@ -1,6 +1,6 @@
 let minValue = parseInt(prompt('Минимальное знание числа для игры','0'));
 let maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
-alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
+alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю за 7 попыток`);
 let answerNumber  = Math.floor((minValue + maxValue) / 2);
 let orderNumber = 1;
 let gameRun = true;
@@ -10,12 +10,6 @@ const answerField = document.getElementById('answerField');
 
 orderNumberField.innerText = orderNumber;
 answerField.innerText = `Вы загадали число ${answerNumber }?`;
-
-document.getElementById('btnRetry').addEventListener('click', function () {
-    minValue = 0;
-    maxValue = 100;
-    orderNumber = 0;
-})
 
 document.getElementById('btnOver').addEventListener('click', function () {
     if (gameRun){
@@ -32,15 +26,86 @@ document.getElementById('btnOver').addEventListener('click', function () {
             answerNumber  = Math.floor((minValue + maxValue) / 2);
             orderNumber++;
             orderNumberField.innerText = orderNumber;
-            answerField.innerText = `Вы загадали число ${answerNumber }?`;
+            const phraseRandom = Math.round( Math.random() * 3);
+                switch (phraseRandom) {
+                    case 1:
+                        answerPhrase = `Я думаю, что это ${answerNumber }?`;  
+                        break;
+                    case 2:
+                        answerPhrase = `Я уже близко. Это ${answerNumber }?`;
+                        break;
+                    case 3:  
+                        answerPhrase = `Мне кажется, что ваше число ${answerNumber }?`;
+                        break;  
+                    default: 
+                        answerPhrase = `Вы загадали число ${answerNumber }?`;
+                 }  
+            answerField.innerText = answerPhrase;
+        }
+    }
+})
+
+document.getElementById('btnLess').addEventListener('click', function () {
+    if (gameRun){
+        if (minValue === maxValue){
+            const phraseRandom = Math.round( Math.random());
+            const answerPhrase = (phraseRandom === 1) ?
+                `Вы загадали неправильное число!\n\u{1F914}` :
+                `Я сдаюсь..\n\u{1F92F}`;
+
+            answerField.innerText = answerPhrase;
+            gameRun = false;
+        } else {
+            maxValue = answerNumber - 1;
+            answerNumber  = Math.floor((minValue + maxValue) / 2);
+            orderNumber++;
+            orderNumberField.innerText = orderNumber;
+            const phraseRandom = Math.round( Math.random() * 3);
+                switch (phraseRandom) {
+                    case 1:
+                        answerPhrase = `Я думаю, что это ${answerNumber }?`;  
+                        break;
+                    case 2:
+                        answerPhrase = `Я уже близко. Это ${answerNumber }?`;
+                        break;
+                    case 3:  
+                        answerPhrase = `Мне кажется, что ваше число ${answerNumber }?`;
+                        break;  
+                    default: 
+                        answerPhrase = `Вы загадали число ${answerNumber }?`;
+                 }  
+            answerField.innerText = answerPhrase;
         }
     }
 })
 
 document.getElementById('btnEqual').addEventListener('click', function () {
     if (gameRun){
-        answerField.innerText = `Я всегда угадываю\n\u{1F60E}`
+        const phraseRandom = Math.round( Math.random() * 3);
+        switch (phraseRandom) {
+            case 1:
+                answerPhrase = `Я всегда угадываю\n\u{1F60E}`;    
+                break;
+            case 2:
+                answerPhrase = `Я так и знал!`; 
+                break;
+            case 3:  
+                answerPhrase = `Да я просто волшебник!`;
+                break;  
+            default: 
+                answerPhrase = `Могу, умею, практикую. Еще раз?`;
+        }  
+        answerField.innerText = answerPhrase; 
         gameRun = false;
     }
 })
 
+document.getElementById('btnRetry').addEventListener('click', function () {
+    orderNumber = 0;
+    minValue = parseInt(prompt('Минимальное знание числа для игры','0'));
+    maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
+    alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его снова угадаю`); 
+    answerNumber  = Math.floor((minValue + maxValue) / 2);
+    answerField.innerText = `Наверное, это число ${answerNumber }?`;
+    gameRun = true;
+ })
